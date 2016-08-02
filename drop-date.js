a904d738f5417891b3625d7b10d5869396e390eb
Adam <afaherty27@gmail.com>
2016-08-02 14:03:28 -0500

    added datepicker to start date.  formatted date

---
+++ drop-date.js
@@ -0,0 +1,10 @@
+//STEP1
+$(document).ready(function() {
+
+	//STEP2
+	//datepicker on start date
+    $("#startdate").datepicker({dateFormat: 'yy-mm-dd',changeMonth: true, changeYear: true});
+	
+	//STEP 3
+	//datepicker on end date
+});

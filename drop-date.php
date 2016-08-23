3832ce41f6c1f897621b7b429f27584aeb10bce8
afaherty27 <afaherty27@gmail.com>
2016-08-23 15:12:52 -0500

    added logo.  adjusted css on bottom paragraph

--- drop-date.php
+++ drop-date.php
@@ -11,7 +11,10 @@
   <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css" />
 </head>
 <body>
+
+	<img alt="logo" id="logo" src="logo.png" />
 	<h1>Refund Estimation Calculator</h1>
+	
 	<form method="post" action="drop-date-calculator.php">
 		<h4 id="instructions"> Please provide the following information:</h4>
 		<label class="blurb"for="startdate" >Course Start Date</label>
@@ -23,7 +26,7 @@
 		<input id="calc" type="submit" value="Calculate" name="submit" />
 	</form>
 	
-	<p id="disclaimer" style="margin-top: 40.52%">
+	<p id="disclaimer" style="margin-top: 35.3%">
 			This refund estimation calculator is provided to offer guidance and
 			estimation of refund levels based on the start and end date of a 
 			class as indicated in the class scheduling system.  Actual refunds 

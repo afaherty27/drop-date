17ee9f259e78b34138cff35ab8897a4083adc6e3
afaherty27 <afaherty27@gmail.com>
2016-08-23 10:49:12 -0500

    removed debug code

--- drop-date-window.js
+++ drop-date-window.js
@@ -5,9 +5,8 @@
 
 $(document).ready(function() {
 
-	//demo.html id for link is dropDate
-	$("#dropDate").click(function(){
-		alert("clicked button");
+	//open app in new window
+	$("#dropDate").click(function(){		
 		window.open('drop-date-calculator.html', 'newwindow', 
 				config='height=600, width=350, toolbar=no,' 
 				+ 'menubar=no, scrollbars=no, resizable=no, location=no, '

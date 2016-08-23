58db47df4ba2a5929a91371dddc4647dd7537af7
afaherty27 <afaherty27@gmail.com>
2016-08-23 10:40:59 -0500

    window configured

--- drop-date-window.js
+++ drop-date-window.js
@@ -2,3 +2,15 @@
 	Adam Faherty
 	JS file to configure window size of php drop-date calculator window
 */
+
+$(document).ready(function() {
+
+	//demo.html id for link is dropDate
+	$("#dropDate").click(function(){
+		alert("clicked button");
+		window.open('drop-date-calculator.html', 'newwindow', 
+				config='height=600, width=350, toolbar=no,' 
+				+ 'menubar=no, scrollbars=no, resizable=no, location=no, '
+				+ 'directories=no, status=no');
+	});
+});

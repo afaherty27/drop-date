051ad92d656990e7f0bf9146b515d91c49baa590
afaherty27 <afaherty27@gmail.com>
2016-08-29 16:24:18 -0500

    missing return boolean added

--- drop-date.js
+++ drop-date.js
@@ -14,25 +14,21 @@ $(document).ready(function() {
 */
 function validation() {
 	
-	
-	
-	//alert($("#startdate").datepicker("getDate");
 	var startDate = $("#startdate").datepicker({ dateFormat: 'MM' }).val();
-	var endDate   = $("#enddate").datepicker({ dateFormat: 'dd, MM, yyyy' }).val();
-	//alert($("#startdate").datepicker({ dateFormat: 'dd, MM, yyyy' }).val());
-	
+	var endDate   = $("#enddate").datepicker({ dateFormat: 'dd, MM, yyyy' }).val();	
 	
 	if (startDate > endDate) {
 		alert("testing intentional fail has been successfull");
 		$("#startdate").value = "";
 		$("#enddate").value = "";
 		
-		
-	} 
-		
+		return false;
+	}
+			
 	// validate that end date is after start date
 	
 	// validate dates are not the same date
 	
-	
+	// AJAX call to run calculator
+
 }

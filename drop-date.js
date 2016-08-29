174aa4bf17ba7fa24846131cdc266315d048c312
afaherty27 <afaherty27@gmail.com>
2016-08-29 16:17:41 -0500

    recieve selected dates during submit.  conditional logic if improper information is sent

--- drop-date.js
+++ drop-date.js
@@ -13,5 +13,26 @@ $(document).ready(function() {
 	Validates the user input
 */
 function validation() {
-	alert('test'); //TEST STUB
+	
+	
+	
+	//alert($("#startdate").datepicker("getDate");
+	var startDate = $("#startdate").datepicker({ dateFormat: 'MM' }).val();
+	var endDate   = $("#enddate").datepicker({ dateFormat: 'dd, MM, yyyy' }).val();
+	//alert($("#startdate").datepicker({ dateFormat: 'dd, MM, yyyy' }).val());
+	
+	
+	if (startDate > endDate) {
+		alert("testing intentional fail has been successfull");
+		$("#startdate").value = "";
+		$("#enddate").value = "";
+		
+		
+	} 
+		
+	// validate that end date is after start date
+	
+	// validate dates are not the same date
+	
+	
 }
